f51200d3b0f6e0ff750109c77464061c3ec04cb4
Jordan Jones <jordan.jones@mintellity.com>
2023-08-08 22:30:04 -0400

    made grid

---
+++ script.js
@@ -0,0 +1,10 @@
+const styleddiv = document.getElementById("container");
+
+for (i = 0; i < 16; ++i ) {
+    const gridplace = document.createElement("div");
+    gridplace.classList.add("gridplace");
+    styleddiv.appendChild(gridplace);
+
+
+}
+
